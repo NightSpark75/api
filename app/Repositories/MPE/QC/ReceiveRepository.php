85556778142b186b00070b0afce9afbeb4346a91
spark <Lin.yupin@stantard.com.tw>
2017-09-08 15:28:20 +0800

    修正QC領料需求

--- app/Repositories/MPE/QC/ReceiveRepository.php
+++ app/Repositories/MPE/QC/ReceiveRepository.php
@@ -117,11 +117,26 @@ class ReceiveRepository
                         'valid' => $item->valid,
                         'buydate' => $item->buydate,
                     ]);
-
                     DB::update("
-                        update mpe_house_e h
-                            set sta = 'Y', amt = 0
-                        where barcode = :barcode
+                        update mpe_house_e e
+                        set sta = 'Y'
+                          , opdate = to_number(to_char(sysdate, 'YYYYMMDD'))
+                          , opvl = (
+                            select 
+                            case 
+                              when e.valid > (
+                                case 
+                                  when typ = 'A' then to_number(to_char(add_months(sysdate, 24)-1, 'YYYYMMDD'))
+                                  when typ = 'B' then to_number(to_char(add_months(sysdate, 12)-1, 'YYYYMMDD'))
+                                end) 
+                                then (case when typ = 'A' then to_number(to_char(add_months(sysdate, 24)-1, 'YYYYMMDD'))
+                                  when typ = 'B' then to_number(to_char(add_months(sysdate, 12)-1, 'YYYYMMDD')) end)
+                              else e.valid 
+                            end
+                            from mpe_mate m
+                            where e.partno = m.partno
+                          )
+                        where e.barcode = :barcode
                     ", ['barcode' => $item->barcode]);
                 }
             });
