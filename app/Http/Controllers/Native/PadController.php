b52db06f631b2dbec7bfdab173a47c38d7c253f4
spark <Lin.yupin@stantard.com.tw>
2017-10-03 09:58:24 +0800

    fix bundle file length

--- app/Http/Controllers/Native/PadController.php
+++ app/Http/Controllers/Native/PadController.php
@@ -26,7 +26,9 @@ class PadController extends Controller
             $decode = base64_decode($file);
             $name = 'index.android.bundle';
     
-            $response = response($decode)->header('Content-Disposition', 'attachment; filename=' . $name);
+            $response = response($decode)
+                ->header('Content-length', strlen($file))
+                ->header('Content-Disposition', 'attachment; filename=' . $name);
             return $response;
         }
         return $result['msg'];
