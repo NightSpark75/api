411d25cce1f493c21366623a5ca2ea84ed264819
spark <Lin.yupin@stantard.com.tw>
2017-10-31 16:05:52 +0800

    complele cors

--- resources/assets/js/components/sys/layout/Login.js
+++ resources/assets/js/components/sys/layout/Login.js
@@ -59,12 +59,11 @@ export default class Login extends React.Component{
         form_data.append('account', account);
         form_data.append('password', password);
         form_data.append('system', system);
-        axios.post('/api/web/login', form_data, {
-            method: 'post',
-        }).then(function (response) {
+        axios.post('http://172.17.100.51/api/web/nativeLogin', form_data)
+        .then(function (response) {
             if (response.data.result === true) {
                 console.log(response.data);
-                window.location = '/auth/web/menu';
+                //window.location = '/auth/web/menu';
             } else {
                 self.setMsg('danger', response.data.msg);
                 self.setState({buttonState: ''});
