c16fe6b7c84358b314db874b4af75f561d480c70
spark <Lin.yupin@stantard.com.tw>
2018-02-05 15:05:14 +0800

    fix navbar

--- resources/assets/js/components/sys/layout/NavPage.js
+++ resources/assets/js/components/sys/layout/NavPage.js
@@ -45,29 +45,20 @@ export default class NavPage extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar">
+        <nav className="navbar is-primary">
           <div className="container">
-            <div className="navbar-brand">
-              <div className="navbar-item">
-                <label className="label is-medium has-text-white-ter">
+            <div className="navbar-start">
+              <div className="nav-item">
                   {this.state.user.length === 0 ? '資料讀取中...' : this.state.user['user_name'] + ' 您好'}
-                </label>
-              </div>
-              <div className="navbar-burger burger" data-target="navMenu" onClick={this.navMenu.bind(this)}>
-                <span></span>
-                <span></span>
-                <span></span>
               </div>
             </div>
-            <div id="navMenu" className={this.state.nav + " navbar-menu"}>
-              <div className="navbar-end">
-                <Link className="navbar-item" to="/auth/web/menu">
-                  <span className="is-size-4">回功能頁</span>
-                </Link>
-                <a className="navbar-item" onClick={this.onLogout.bind(this)} href="#">
-                  <span className="is-size-4">登出</span>
-                </a>
-              </div>
+            <div className="navbar-end">
+              <Link className="navbar-item" to="/auth/web/menu">
+                <span className="is-size-4">回功能頁</span>
+              </Link>
+              <a className="navbar-item" onClick={this.onLogout.bind(this)} href="#">
+                <span className="is-size-4">登出</span>
+              </a>
             </div>
           </div>
         </nav>
