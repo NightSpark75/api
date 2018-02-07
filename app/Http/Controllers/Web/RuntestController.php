e9107e5bbe020c0af83e846c8a3137861bd5d47e
spark <Lin.yupin@stantard.com.tw>
2018-02-07 14:43:43 +0800

    test

--- app/Http/Controllers/Web/RuntestController.php
+++ app/Http/Controllers/Web/RuntestController.php
@@ -21,10 +21,17 @@ class RuntestController extends Controller
     public function test()
     {
         //$a = $this->overdue->overdueNotice();
-        $a = DB::select("
-            select *
-                from mpz_point
+        $list = DB::select("
+            select m.no, m.doc_class, m.reason, e.barcode, e.partno, pk_mpe.fu_pname(e.partno) pname
+                    , e.bno, m.apply_user, stdadm.pk_hra.fu_emp_name(m.apply_user) apply_ename
+                    , stdadm.pk_hra.fu_emp_email(m.apply_user) aemail
+                    , m.receive_user, stdadm.pk_hra.fu_emp_name(m.apply_user) receive_ename
+                    , stdadm.pk_hra.fu_emp_email(m.receive_user) remail, m.rdate
+                from mpe_lsa_e e, mpe_lsa_m m 
+                where e.status = 'R' and e.lsa_no = m.no 
+                    and pk_date.fu_number(sysdate) >= stdadm.pk_hra.fu_work_day('ALL', m.rdate, 7)
+                order by m.no, m.rdate
         ");
-        return $a;
+        return $list;
     }
 }
