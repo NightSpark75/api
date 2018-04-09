e4d4443abce599d9796dd7ea2c1280e753ab0ee1
spark <Lin.yupin@stantard.com.tw>
2018-04-09 09:49:05 +0800

    modify jwt message to chinese

--- app/Services/Web/JwtService.php
+++ app/Services/Web/JwtService.php
@@ -30,16 +30,16 @@ class JwtService {
             // attempt to verify the credentials and create a token for the user
             $user = $this->userRepository->authUser($id, $password);
             if (!$user) {
-                throw new Exception('invalid credentials');
+                throw new Exception('帳號或密碼錯誤!');
             }
             $payload = ['name' => $user->name];
             if (!$token = JWTAuth::fromUser($user, $payload)) {
-                throw new Exception('invalid credentials');
+                throw new Exception('Token建立失敗!');
             }
             return $token;
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            throw new Exception('could not create token');
+            throw new Exception('伺服器錯誤:無法建立Token('.$e->getMessage().')');
         }
     }
 
@@ -50,10 +50,10 @@ class JwtService {
                 $refresh = JWTAuth::refresh($token);
                 return $refresh;
             }
-            throw new Exception('invalid token');
+            throw new Exception('不合法的Token');
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            throw new Exception('could not create token');
+            throw new Exception('伺服器錯誤:無法建立Token('.$e->getMessage().')');
         }
     }
 }
