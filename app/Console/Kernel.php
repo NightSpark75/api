fe778bcbb674305b7ccfc5bc2b2d743d2409e6e3
spark <Lin.yupin@stantard.com.tw>
2018-04-16 15:43:17 +0800

    fix bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,6 @@ class Kernel extends ConsoleKernel
         //$schedule->command('Web:MPZ:expired')->dailyAt('07:00');
         $schedule->command('Web:MPZ:Overdue0830')->dailyAt('08:30');
         $schedule->command('Web:MPZ:Overdue1330')->dailyAt('13:30');
-        $schedule->command('Web:MPZ:Overdue1330')->dailyAt('15:40');
         $schedule->command('Web:MPZ:Overdue1700')->dailyAt('17:00');
         $schedule->command('Web:MPZ:Overdue2200')->dailyAt('22:00');
     }
